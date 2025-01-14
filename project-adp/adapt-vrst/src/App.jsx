0ee44b3ebc37963b9a57403ae41b1d07b975a915
Your Name <you@example.com>
2025-01-14 23:40:37 +0500

    px in css

--- project-adp/adapt-vrst/src/App.jsx
+++ project-adp/adapt-vrst/src/App.jsx
@@ -2,6 +2,7 @@ import MainContentPage from './components/Main-content/MainContentPage'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import MainContentPage2 from './components/Main-content/MainContentPage2'
+
 export default function App() {
   
 
